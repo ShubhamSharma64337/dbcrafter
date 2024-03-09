b78d4fb84e2923f0c27039d530d1fbc33019143a
Shubham Sharma <shubhamsharma64337@gmail.com>
2024-03-09 15:39:41 +0530

    Started designing the modal

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import LoginForm from './LoginForm'
 import SignupForm from './SignupForm'
+import Modal from './Modal'
 export default function MainContent({type, theme, showAlert, authInfo}) {
     return (
-            <div className="flex flex-col justify-center items-center pt-20 md:flex-row">
+        <div className="flex flex-col justify-center items-center pt-20 md:flex-row">
                 <div className="left w-full p-10 md:w-2/3">
                     <p className="text-3xl font-medium text-center" >
                         Open Source relational database design tool built using ReactJS and HTML Canvas. Create diagrams, use templates, export as SQL or image.
