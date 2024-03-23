f11ffe55230525fcb3b150b35ba0c56985ddf173
Shubham Sharma <shubhamsharma64337@gmail.com>
2024-03-23 09:15:35 +0530

    Fixed the back button of maincanvas

--- src/components/MainCanvas.jsx
+++ src/components/MainCanvas.jsx
@@ -552,7 +552,7 @@ export default function MainCanvas({showAlert, theme, authInfo, diagram, setDiag
                     </button>
                 </div>
                 <div className="top-left-buttons flex flex-col fixed top-4 left-4 gap-5">
-                    <Link to={'/diagrams'} type='button' className={`bg-blue-700 flex shadow-lg p-3 text-white transition-transform rounded-full hover:scale-110`} onClick={() => { }}>
+                    <Link to={!authInfo?'/':'/diagrams'} type='button' className={`bg-blue-700 flex shadow-lg p-3 text-white transition-transform rounded-full hover:scale-110`}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                             <path fillRule="evenodd" d="M9.53 2.47a.75.75 0 0 1 0 1.06L4.81 8.25H15a6.75 6.75 0 0 1 0 13.5h-3a.75.75 0 0 1 0-1.5h3a5.25 5.25 0 1 0 0-10.5H4.81l4.72 4.72a.75.75 0 1 1-1.06 1.06l-6-6a.75.75 0 0 1 0-1.06l6-6a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
                         </svg>
