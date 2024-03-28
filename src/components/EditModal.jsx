ba8c7196c0307990101cbbf756fb862636629cd9
Shubham Sharma <shubhamsharma64337@gmail.com>
2024-03-28 20:18:29 +0530

    Changed the EditModal save button

--- src/components/EditModal.jsx
+++ src/components/EditModal.jsx
@@ -146,11 +146,11 @@ export default function EditModal({table, editShow, toggleEditModal, tbls, showA
                 </svg>
               </button>
               <p className='text-center text-lg font-medium '>Modify Table</p>
-              <button className="bg-slate-200 p-2 rounded-full  transition-colors hover:bg-blue-300" type='button' onClick={editTable}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
-                </svg>
-              </button>
+            <button className="bg-slate-200 p-2 rounded-full  transition-colors hover:bg-blue-300" type='button' onClick={editTable}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                <path fillRule="evenodd" d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z" clipRule="evenodd" />
+              </svg>
+            </button>
             </div>
 
             {/* Modal Body */}
