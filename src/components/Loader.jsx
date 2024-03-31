639d774abecd6a904ab48e4d0d4f6cfb32671cce
Shubham Sharma <shubhamsharma64337@gmail.com>
2024-03-31 16:24:06 +0530

    Updated the design of Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Loader({isLoading}) {
   return (
-    isLoading && <div className="overlay overflow-auto fixed justify-center md:justify-center  flex items-center pt-5 top-0 w-screen h-screen bg-black bg-opacity-35">
-        <div className="loading-ring border-4 border-t-blue-500  p-5 rounded-full">
+    isLoading && <div className="overlay overflow-auto fixed justify-center md:justify-center  flex items-center pt-5 top-0 w-screen h-screen bg-black bg-opacity-75">
+        <div className="loading-ring border-t-4 border-t-blue-500  p-5 rounded-full">
             
         </div>
     </div>
