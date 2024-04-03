2eb47313d67fad4c9f380ff2adc5523b54c8546b
Shubham Sharma <shubhamsharma64337@gmail.com>
2024-04-03 11:50:35 +0530

    Added the ability to retrieve and open the templates as a new unnamed diagram

--- src/components/Diagrams.jsx
+++ src/components/Diagrams.jsx
@@ -281,7 +281,7 @@ export default function Diagrams({authInfo, showAlert, setDiagram, setIsLoading,
                         </svg>
                     </button>
           </div>
-          <TemplateModal visible={templateModalVisible} toggleVisible={toggleTemplateModal} templates={templates}></TemplateModal>
+          <TemplateModal showAlert={showAlert} urls={urls} visible={templateModalVisible} toggleVisible={toggleTemplateModal} templates={templates} setIsLoading={setIsLoading} setDiagram={setDiagram}></TemplateModal>
     </div>
     :
         <div className='flex justify-center items-center gap-5 p-10 flex-col'>
@@ -301,8 +301,14 @@ export default function Diagrams({authInfo, showAlert, setDiagram, setIsLoading,
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                         </svg>
                     </button>
+                    <button type='button' className={`group relative bg-blue-700 flex shadow-lg p-3 text-white transition-transform rounded-full hover:scale-110`} onClick={openTemplateModal}>
+                        <span className={`text-sm text-nowrap tooltip absolute right-full top-1/2 bg-white text-black border border-slate-500 px-2 py-1 rounded -translate-y-1/2 me-2 hidden group-hover:block`}>New From Template</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                          <path fillRule="evenodd" d="M9 4.5a.75.75 0 0 1 .721.544l.813 2.846a3.75 3.75 0 0 0 2.576 2.576l2.846.813a.75.75 0 0 1 0 1.442l-2.846.813a3.75 3.75 0 0 0-2.576 2.576l-.813 2.846a.75.75 0 0 1-1.442 0l-.813-2.846a3.75 3.75 0 0 0-2.576-2.576l-2.846-.813a.75.75 0 0 1 0-1.442l2.846-.813A3.75 3.75 0 0 0 7.466 7.89l.813-2.846A.75.75 0 0 1 9 4.5ZM18 1.5a.75.75 0 0 1 .728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 0 1 0 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 0 1-1.456 0l-.258-1.036a2.625 2.625 0 0 0-1.91-1.91l-1.036-.258a.75.75 0 0 1 0-1.456l1.036-.258a2.625 2.625 0 0 0 1.91-1.91l.258-1.036A.75.75 0 0 1 18 1.5ZM16.5 15a.75.75 0 0 1 .712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 0 1 0 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 0 1-1.422 0l-.395-1.183a1.5 1.5 0 0 0-.948-.948l-1.183-.395a.75.75 0 0 1 0-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0 1 16.5 15Z" clipRule="evenodd" />
+                        </svg>
+                    </button>
           </div>
-          <TemplateModal visible={templateModalVisible} toggleVisible={toggleTemplateModal} templates={templates}></TemplateModal>
+          <TemplateModal showAlert={showAlert} urls={urls} visible={templateModalVisible} toggleVisible={toggleTemplateModal} templates={templates} setIsLoading={setIsLoading} setDiagram={setDiagram}></TemplateModal>
         </div>
   )
 }
--- src/components/TemplateModal.jsx
+++ src/components/TemplateModal.jsx
@@ -1,6 +1,39 @@
 import React, { useEffect, useState } from 'react'
+import {  useNavigate } from "react-router-dom";
 
-export default function TemplateModal({visible, toggleVisible, templates}) {
+export default function TemplateModal({visible, toggleVisible, templates, setIsLoading, setDiagram, urls, showAlert}) {
+  const navigate = useNavigate();
+  function openTemplate(e){
+    setIsLoading(true);
+    fetch(import.meta.env.PROD?urls.productionUrl+'/user/gettemplate':urls.devUrl+'/user/gettemplate', {
+        method: 'POST',
+        headers: {         
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include', //this must be set in order to save the received session-cookie,
+        //also, after setting credentials to include, cors options must be set to allow credentials and origin from this domain
+        body: JSON.stringify({_id:e.currentTarget.dataset.id})
+      })
+      .then(response => response.json()) //response.json() or response.text() provides the 'data'
+      .then((data)=>{
+          if(data.success){
+            setDiagram({name: null, isPublic: false, tbls: data.message.tbls})
+            navigate('/craft');
+            return;
+          } else {
+            showAlert(data.message, 'danger');
+            return;
+          }
+      })
+      .catch((error)=>{
+        showAlert('An error occured while trying to access the backend API', 'danger')
+        console.log(error)
+      })
+      .finally(()=>{
+        setIsLoading(false);
+      })
+  }
+
   return (
     visible && templates && <div className="overlay overflow-auto fixed justify-center  flex items-start p-5 top-0 left-0 w-screen h-screen bg-black bg-opacity-75">
         <div className="modal relative bg-white rounded pt-10 w-full">
@@ -14,8 +47,8 @@ export default function TemplateModal({visible, toggleVisible, templates}) {
 
             {/* Modal Body */}
             <div className="modal-body grid grid-cols-3 gap-5 p-5">
-              {templates? templates.map((element)=>{
-                return <button className='cardf grid grid-cols-1 grid-rows-4 bg-white rounded hover:bg-sky-100 px-5 py-4 border border-slate-200'>
+              {templates? templates.map((element, index)=>{
+                return <button key={index} className='card grid grid-cols-1 grid-rows-4 bg-white rounded hover:bg-sky-100 px-5 py-4 border border-slate-200' onClick={openTemplate} data-id={element._id}>
                   <div className='image row-span-3 flex justify-center items-center px-5 py-20'>
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                     <path fillRule="evenodd" d="M11.097 1.515a.75.75 0 0 1 .589.882L10.666 7.5h4.47l1.079-5.397a.75.75 0 1 1 1.47.294L16.665 7.5h3.585a.75.75 0 0 1 0 1.5h-3.885l-1.2 6h3.585a.75.75 0 0 1 0 1.5h-3.885l-1.08 5.397a.75.75 0 1 1-1.47-.294l1.02-5.103h-4.47l-1.08 5.397a.75.75 0 1 1-1.47-.294l1.02-5.103H3.75a.75.75 0 0 1 0-1.5h3.885l1.2-6H5.25a.75.75 0 0 1 0-1.5h3.885l1.08-5.397a.75.75 0 0 1 .882-.588ZM10.365 9l-1.2 6h4.47l1.2-6h-4.47Z" clipRule="evenodd" />
