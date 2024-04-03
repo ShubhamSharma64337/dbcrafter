53c2784aaf067cbb53cbc7360d8254aad6ca72a4
Shubham Sharma <shubhamsharma64337@gmail.com>
2024-04-03 13:18:15 +0530

    Added password show/hide feature to login form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -3,6 +3,7 @@ import {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 export default function LoginForm({showAlert, setAuthInfo, theme, setIsLoading, urls}) {
   const [mail,setMail] = useState('');
+  const [passwordVisible, setPasswordVisible] = useState(false);
   const navigate = useNavigate();
   function handleLowerMail(event){
     setMail(event.target.value.toLowerCase());
@@ -56,7 +57,25 @@ export default function LoginForm({showAlert, setAuthInfo, theme, setIsLoading, 
           </div>
           <div className="my-5">
               <label htmlFor="loginPassword" className="block">Password</label>
-              <input type="password" required={true} className="border-2 border-slate-300 bg-slate-50 p-2 w-full outline-blue-700 hover:bg-slate-200 transition focus:bg-white" id="loginPassword" placeholder='Enter your password' name="password"/>
+              <div className='flex'>
+                <input type={passwordVisible?'text':'password'} required={true} className="z-10 border-2 border-r-0 border-slate-300 bg-slate-50 p-2 w-full outline-blue-700 hover:bg-slate-200 transition focus:bg-white" id="loginPassword" placeholder='Enter your password' name="password"/>
+                <button type='button' className="px-2 border-2 border-l-0 -z-2 border-slate-300 bg-slate-50 text-slate-500 rounded-r outline-blue-700" onClick={()=>{
+                  passwordVisible?setPasswordVisible(false):setPasswordVisible(true);
+                }}>
+              {passwordVisible ?
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                  <path d="M3.53 2.47a.75.75 0 0 0-1.06 1.06l18 18a.75.75 0 1 0 1.06-1.06l-18-18ZM22.676 12.553a11.249 11.249 0 0 1-2.631 4.31l-3.099-3.099a5.25 5.25 0 0 0-6.71-6.71L7.759 4.577a11.217 11.217 0 0 1 4.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113Z" />
+                  <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0 1 15.75 12ZM12.53 15.713l-4.243-4.244a3.75 3.75 0 0 0 4.244 4.243Z" />
+                  <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 0 0-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 0 1 6.75 12Z" />
+                </svg>
+                :
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6">
+                  <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+                  <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z" clipRule="evenodd" />
+                </svg>
+              }
+              </button>
+              </div>
           </div>
           <div className="my-5 flex items-center">
               <input type="checkbox" className="w-4 h-4" id="rememberCheck"/>
