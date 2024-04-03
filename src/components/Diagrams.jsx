13f848a53ee4817319fb2d810dc21d73d2323bf6
Shubham Sharma <shubhamsharma64337@gmail.com>
2024-04-03 22:30:26 +0530

    When clicking on rename button, mouse now automatically focuses inside the input

--- src/components/Diagrams.jsx
+++ src/components/Diagrams.jsx
@@ -118,13 +118,19 @@ export default function Diagrams({authInfo, showAlert, setDiagram, setIsLoading,
     }
 
     function enableEdit(e){
-      let newDiagrams = diagrams.map((element, index)=>{
+      let newDiagrams = diagrams.map((element)=>{
         if(element._id === e.currentTarget.dataset.diagramid){
           return {...element, isEditing: true};
         }
         return {...element}
       });
       setDiagrams(newDiagrams);
+      let inp = document.getElementById('input'+e.currentTarget.dataset.diagramid);
+      console.log(inp);
+      inp.disabled = false; //this must be done because if we do not make the input enabled, focus() will be called before it is enabled, thus 
+      //the cursor will not be able to move inside the input
+      inp.focus();
+
     }
 
     function disableEdit(e){
@@ -226,7 +232,22 @@ export default function Diagrams({authInfo, showAlert, setDiagram, setIsLoading,
     diagrams ? <div className='p-5 grid gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
           {diagrams.map((element, index)=>{
               return (
-              <div key={index} className="card p-1 rounded-lg border-2 border-blue-50 shadow-lg flex flex-col justify-center items-center transition overflow-hidden hover:shadow hover:border-2 hover:border-blue-400">
+              <div key={index} className="card relative p-1 rounded-lg border-2 border-blue-50 shadow-lg flex flex-col justify-center items-center transition overflow-hidden hover:shadow hover:border-2 hover:border-blue-400">
+                  <div className='publicStatus absolute top-2 right-2 text-gray-400'>
+                        {
+                          element.isPublic?
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6 open-eye">
+                          <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+                          <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z" clipRule="evenodd" />
+                        </svg>
+                        :
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 closed-eye">
+                          <path d="M3.53 2.47a.75.75 0 0 0-1.06 1.06l18 18a.75.75 0 1 0 1.06-1.06l-18-18ZM22.676 12.553a11.249 11.249 0 0 1-2.631 4.31l-3.099-3.099a5.25 5.25 0 0 0-6.71-6.71L7.759 4.577a11.217 11.217 0 0 1 4.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113Z" />
+                          <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0 1 15.75 12ZM12.53 15.713l-4.243-4.244a3.75 3.75 0 0 0 4.244 4.243Z" />
+                          <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 0 0-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 0 1 6.75 12Z" />
+                        </svg>
+                        }
+                  </div>
                   <div className="image flex justify-center items-center px-2 py-10 w-full" data-diagramid={element._id}  onClick={!element.isEditing?openDiagram:undefined}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="text-blue-500 w-10 h-10">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
