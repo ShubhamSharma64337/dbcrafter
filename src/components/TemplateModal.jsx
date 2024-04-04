3f3bc1b00de6f5314f7a7209fe3824d176a725c3
Shubham Sharma <shubhamsharma64337@gmail.com>
2024-04-04 20:03:42 +0530

    Modified the design of templates modal

--- src/components/TemplateModal.jsx
+++ src/components/TemplateModal.jsx
@@ -36,34 +36,38 @@ export default function TemplateModal({visible, toggleVisible, templates, setIsL
 
   return (
     visible && templates && <div className="overlay overflow-auto fixed justify-center  flex items-start p-5 top-0 left-0 w-screen h-screen bg-black bg-opacity-75">
-        <div className="modal relative bg-white rounded pt-10 w-full">
+        <div className="modal bg-white rounded w-full">
             {/* Modal Header */}
-              <button type="button" className="p-2 absolute top-2 right-2 rounded-full transition-colors  hover:bg-red-300" onClick={toggleVisible}>
+            <div className="modal-header p-2 flex justify-between items-center border-b">
+              <div className='text-lg ms-2'>
+                Public Diagrams
+              </div>
+              <button type="button" className="p-2 rounded-full transition-colors  hover:bg-red-300" onClick={toggleVisible}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                 </svg>
               </button>
+            </div>
 
 
             {/* Modal Body */}
-            <div className="modal-body grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 p-5">
-              {templates? templates.map((element, index)=>{
-                return <button key={index} className='card grid grid-cols-1 grid-rows-4 bg-white rounded hover:bg-sky-100 px-5 py-4 border border-slate-200' onClick={openTemplate} data-id={element._id}>
-                  <div className='image row-span-3 flex justify-center items-center px-5 py-20'>
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                    <path fillRule="evenodd" d="M11.097 1.515a.75.75 0 0 1 .589.882L10.666 7.5h4.47l1.079-5.397a.75.75 0 1 1 1.47.294L16.665 7.5h3.585a.75.75 0 0 1 0 1.5h-3.885l-1.2 6h3.585a.75.75 0 0 1 0 1.5h-3.885l-1.08 5.397a.75.75 0 1 1-1.47-.294l1.02-5.103h-4.47l-1.08 5.397a.75.75 0 1 1-1.47-.294l1.02-5.103H3.75a.75.75 0 0 1 0-1.5h3.885l1.2-6H5.25a.75.75 0 0 1 0-1.5h3.885l1.08-5.397a.75.75 0 0 1 .882-.588ZM10.365 9l-1.2 6h4.47l1.2-6h-4.47Z" clipRule="evenodd" />
-                  </svg>
-
+            <div className="modal-body grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-5 p-5">
+              {templates ? templates.map((element, index) => {
+                return <button key={index} className='card grid grid-cols-1 grid-rows-4 bg-white rounded p-1 transition shadow hover:shadow-none hover:border-blue-500 border-2 border-slate-200' onClick={openTemplate} data-id={element._id}>
+                  <div className='image row-span-3 h-full flex justify-center items-center'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
+                    </svg>
                   </div>
-                  <div className='row-span-1 text-lg'>
-                  {element.name}
+                  <div className='row-span-1 bg-blue-100 rounded py-2'>
+                    {element.name}
                   </div>
-                  </button>
+                </button>
               })
-              :
-              'No Templates Found'
-              
-            }
+                :
+                'No Templates Found'
+
+              }
             </div>
         </div>
     </div>
