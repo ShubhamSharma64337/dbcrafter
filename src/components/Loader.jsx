6252333ba8462104e94163b69a123ffecd71ffdf
Shubham Sharma <shubhamsharma64337@gmail.com>
2024-04-04 15:02:57 +0530

    Modified the design of the loader and set its z-index to 10 so that it stays above all elements

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function Loader({isLoading}) {
   return (
-    isLoading && <div className="overlay overflow-auto fixed justify-center md:justify-center  flex items-center pt-5 top-0 w-screen h-screen bg-black bg-opacity-75">
-        <div className="loading-ring border-t-4 border-t-blue-500  p-5 rounded-full">
+    isLoading && <div className="overlay z-10 overflow-auto fixed justify-center md:justify-center  flex items-center pt-5 top-0 w-screen h-screen bg-black bg-opacity-75">
+        <div className="loading-ring border-4 border-transparent border-t-4 border-t-blue-500  p-10 rounded-full">
             
-        </div>
+            </div>
     </div>
   )
 }
