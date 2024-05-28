b7f3e87b5480e8ce40a9ccc136d828cf6e9af0eb
Shubham Sharma <shubhamsharma64337@gmail.com>
2024-05-28 17:10:40 +0530

    Fixed the bug in show/hide password button in Signup Form

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -84,7 +84,7 @@ export default function SignupForm({showAlert, theme, setIsLoading, urls}) {
                 <button type='button' className="px-2 border-2 border-l-0 -z-2 border-slate-300 bg-slate-50 text-slate-500 rounded-r outline-blue-700" onClick={()=>{
                   confirmPasswordVisible?setConfirmPasswordVisible(false):setConfirmPasswordVisible(true);
                 }}>
-              {passwordVisible ?
+              {confirmPasswordVisible ?
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6 open-eye">
                   <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
                   <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z" clipRule="evenodd" />
