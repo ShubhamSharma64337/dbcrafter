560cf230c53321f2dd5194d02bda988edc9e6f99
Shubham Sharma <shubhamsharma64337@gmail.com>
2024-06-11 06:59:06 +0530

    Updated the style of page number bar

--- src/components/PageNumber.jsx
+++ src/components/PageNumber.jsx
@@ -5,10 +5,10 @@ export default function PageNumber({numPages,currentPage,getdiagrams}) {
         getdiagrams(parseInt(e.currentTarget.value));
   }
   return (
-    <div className='flex justify-center text-lg sm:col-span-2 lg:col-span-3 xl:col-span-4'>
-        <ul className='flex bg-white h-min rounded overflow-hidden border-2 border-blue-300'>
+    <div className='flex justify-center text-sm md:text-md lg:text-lg sm:col-span-2 lg:col-span-3 xl:col-span-4'>
+        <ul className='flex bg-white h-min rounded overflow-hidden shadow'>
             {[...Array(numPages)].map((page,i)=>{
-                return <li key={i}><button className={`py-1 px-3 hover:bg-blue-300 ${currentPage==i+1?'bg-blue-300':''}`} onClick={getpage} value={i+1}>{i+1}</button></li>
+                return <li key={i}><button className={`py-1 px-3 text-slate-800 hover:bg-blue-300 ${currentPage==i+1?'bg-blue-300':''}`} onClick={getpage} value={i+1}>{i+1}</button></li>
             })}
         </ul>
     </div>
