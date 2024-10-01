ff5fe80fb19fe52833f633fc3a377bb4de45d863
Shubham Sharma <shubhamsharma64337@gmail.com>
2024-10-01 12:42:57 +0530

    Fixed infinite loading

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Loader({theme, isLoading}) {
   return (
-    true && <div className={`overlay z-10 overflow-auto fixed justify-center md:justify-center  flex flex-col gap-y-2 items-center pt-5 top-0 w-screen h-screen ${theme==='dark'?'bg-black':'bg-white'} bg-opacity-75`}>
+    isLoading && <div className={`overlay z-10 overflow-auto fixed justify-center md:justify-center  flex flex-col gap-y-2 items-center pt-5 top-0 w-screen h-screen ${theme==='dark'?'bg-black':'bg-white'} bg-opacity-75`}>
         <div className="loading-ring border-4 border-transparent border-t-4 border-t-blue-500  p-10 rounded-full">
             
         </div>
