4ebeb7be26e0f00aa7eb67c85c86be9258acd6d9
Shubham Sharma <shubhamsharma64337@gmail.com>
2024-10-01 12:41:57 +0530

    Added note in loader for high loading time on first visit due to render suspend

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 export default function Loader({theme, isLoading}) {
   return (
-    isLoading && <div className={`overlay z-10 overflow-auto fixed justify-center md:justify-center  flex items-center pt-5 top-0 w-screen h-screen ${theme==='dark'?'bg-black':'bg-white'} bg-opacity-75`}>
+    true && <div className={`overlay z-10 overflow-auto fixed justify-center md:justify-center  flex flex-col gap-y-2 items-center pt-5 top-0 w-screen h-screen ${theme==='dark'?'bg-black':'bg-white'} bg-opacity-75`}>
         <div className="loading-ring border-4 border-transparent border-t-4 border-t-blue-500  p-10 rounded-full">
             
-            </div>
+        </div>
+        <div className="note p-1 bg-white border rounded"><strong>Note - </strong>It may take around 50s to load on first visit</div>
     </div>
   )
 }
